411115d0aa4df1c36cd67d4bc081acefae19a218
Justin Weiss <justin@justinweiss.com>
2017-04-04 20:00:00 -0700

    Added a word selector and sentence generator.

--- markov_generator.rb
+++ markov_generator.rb
@@ -13,6 +13,28 @@ class MarkovGenerator
     dictionary
   end
 
+  def generate_sentence(current_word)
+    sentence = ""
+    until(current_word.chars.last.match(/[.!]/))
+      current_word = next_word(current_word)
+      sentence += " " + current_word
+    end
+    puts sentence.lstrip
+  end
+
+  private
+  
+  def next_word(current_word)
+    if word_frequency = @dictionary[current_word]
+      weighted_random(word_frequency)
+    else
+      next_word(@dictionary.keys.sample)
+    end
+  end
+
+  def weighted_random(words_and_frequencies)
+    words_and_frequencies.map { |word, frequency| [word] * frequency }.flatten.sample
+  end
 end
 
 mv = MarkovGenerator.new
