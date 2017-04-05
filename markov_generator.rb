cd67eeaed603e87f1d4bd31202dd4900347b0b3b
Jake Sparling <jsparling@avvo.com>
2017-04-04 19:57:38 -0700

    read file

--- markov_generator.rb
+++ markov_generator.rb
@@ -2,20 +2,27 @@ class MarkovGenerator
   attr_accessor :dictionary
 
   def initialize
-
   end
 
-  def add_data()
-    @dictionary = [["name", 12], ["blah", 3]]
+
+  def add_data(filename)
+    words = word_array(filename)
   end
 
   def new_text
     dictionary
   end
 
+  private
+
+  def word_array(filename)
+    File.open( filename ){ |f|  f.read.split }
+  end
+
 end
 
 mv = MarkovGenerator.new
-mv.add_data
+
+mv.add_data(ARGV.first)
 
 puts mv.new_text
