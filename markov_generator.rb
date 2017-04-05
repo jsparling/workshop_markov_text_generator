2a927d772097a8e98b1975eadbf12fcb6fd93752
Jake Sparling <jsparling@avvo.com>
2017-04-04 19:48:29 -0700

    framework

--- markov_generator.rb
+++ markov_generator.rb
@@ -1,2 +1,21 @@
+class MarkovGenerator
+  attr_accessor :dictionary
 
+  def initialize
 
+  end
+
+  def add_data()
+    @dictionary = [["name", 12], ["blah", 3]]
+  end
+
+  def new_text
+    dictionary
+  end
+
+end
+
+mv = MarkovGenerator.new
+mv.add_data
+
+puts mv.new_text
