516cffe565990527e2b086e6ef0720ec24bf66a6
Jake Sparling <jsparling@avvo.com>
2017-04-04 20:03:09 -0700

    create hash

--- markov_generator.rb
+++ markov_generator.rb
@@ -7,6 +7,7 @@ class MarkovGenerator
 
   def add_data(filename)
     words = word_array(filename)
+    create_word_hash(words)
   end
 
   def new_text
@@ -14,6 +15,13 @@ class MarkovGenerator
   end
 
   private
+
+  def create_word_hash(array_of_words)
+    return_hash = {}
+    array_of_words.each do |word|
+      put word
+    end
+  end
 
   def word_array(filename)
     File.open( filename ){ |f|  f.read.split }
@@ -25,4 +33,4 @@ mv = MarkovGenerator.new
 
 mv.add_data(ARGV.first)
 
-puts mv.new_text
+# puts mv.new_text
